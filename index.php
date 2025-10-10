4affe3958b25584e71f298e8b92ffeaff7cedf79
IntegerVar <tonna.nich@gmail.com>
2025-10-10 16:51:12 +0200

    added in comments for more readability

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
-require 'vendor/autoload.php';
+require 'vendor/autoload.php'; # Load in required composer packages, including twig
 
+# Load in Twig templates, located in the 'views' folder
 $loader = new \Twig\Loader\FilesystemLoader('views');
 $twig = new \Twig\Environment($loader);
 
+
+# Render in the signup/login page
 echo $twig->render("Signup-Login.html", array(
 
     "WebsiteName" => "TrackyTask - Your To Do List Tracker!",
