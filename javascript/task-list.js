fc4ba04dd5332031c46566fe44696a3ff2c28e0d
IntegerVar <tonna.nich@gmail.com>
2025-10-12 18:35:10 +0200

    created menu templates + injection code for them
    added templates for add task, modify task, and delete task menus
    
    added a function which injects these templates to a menu container

--- javascript/task-list.js
+++ javascript/task-list.js
@@ -1,8 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Event listeners for buttons
-    document.getElementById("addTask").addEventListener("click", addTaskMenuInjector);
 
+    // Given a menuContainer ID & template ID, places the content of the template into the menuContainer
+    function injectMenuToDiv(menuContainerID, templateID) {
+
+        // Select Menu to Modify
+        const menuContainer = document.querySelector("#" + menuContainerID); // # String concat to keep parameters consistent
+        
+        // Select Template To Inject
+        const templateToBeInjected = document.getElementById(templateID);
+
+        // Inject Template Into Menu
+        menuContainer.innerHTML = templateToBeInjected.innerHTML;
+    }
+
+    // Button Objects
+    var addTaskButton = document.getElementById("addTask");
+    var modifyTaskButton = document.getElementById("modifyTask");
+    var deleteTaskButton = document.getElementById("deleteTask");
+    
+    // Event listeners for buttons
+    addTaskButton.addEventListener("click", addTaskMenuInjector);
+    modifyTaskButton.addEventListener("click", modifyTaskMenuInjector);
+    deleteTaskButton.addEventListener("click", deleteTaskMenuInjector);
+
+    // Menu injectors depending on buttons pressed
     function addTaskMenuInjector() {
-    alert("Clicked");
-    }   
+
+        injectMenuToDiv("menuContainer", "addTaskMenuTemplate");
+
+    }
+
+    function modifyTaskMenuInjector() {
+
+        injectMenuToDiv("menuContainer", "modifyTaskMenuTemplate");
+
+    }
+
+    function deleteTaskMenuInjector() {
+
+        injectMenuToDiv("menuContainer", "deleteTaskMenuTemplate");
+
+    }
 });
--- templates/task-list-template.php
+++ templates/task-list-template.php
@@ -21,8 +21,24 @@
         </table>
 
         <button id="addTask">Add Task</button>
-        <button id="deleteTask">Delete Task</button>
         <button id="modifyTask">Modify Task</button>
+        <button id="deleteTask">Delete Task</button>
 
+        <div id="menuContainer"></div>
+
+        <!-- Template content for menuContainer which is controlled with javascript -->
+
+        
+        <template id="addTaskMenuTemplate"> <!-- Add Task Menu-->
+            <p>Add</p>
+        </template>
+
+        <template id="modifyTaskMenuTemplate"> <!-- Modify Task Menu-->
+            <p>Modify</p>
+        </template>
+
+        <template id="deleteTaskMenuTemplate"> <!-- Delete Task Menu-->
+            <p>Delete</p>
+        </template>
     </body>
 </html>
