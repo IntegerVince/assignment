8751ba2db3e0f2adb84863dbb91ecd42e75955eb
IntegerVar <tonna.nich@gmail.com>
2025-10-12 14:16:39 +0200

    added toggler between signup and login

--- templates/signup-login-template.php
+++ templates/signup-login-template.php
@@ -23,7 +23,19 @@
 
         {% elseif templateType == "Signup" %}
 
-            Hello!
+            <h1>{{ websiteName }}, Your To Do List Tracker</h1>
+            <h2>Signup</h2>
+
+            <form action="pages/task-list.php" method="post">
+                <label for="fname">Username:</label>
+                <input type="text" id="fname" name="fusername" placeholder="exampleUser">
+                <label for="fpass">Password:</label>
+                <input type="password" id="fpass" name="fpassword">
+                <input type="submit" value="Submit">
+            </form>
+
+            <!-- Empty "" will refresh the page, which will serve index.php again, which will load in the login template by default -->
+            <a href=""><p>Already have an account? Login</p></a>
 
         {% endif %}
         
