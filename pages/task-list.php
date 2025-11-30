d0a603ff3e00c7924a3210f4a8549eb3cb1e0989
IntegerVar <tonna.nich@gmail.com>
2025-11-30 19:38:53 +0100

    inclusion of headers which define allowed sources
    added headers which includes allowed sources for fonts, apis, images, etc..to reflect actual needs (self, google fonts, google recaptcha api and giphy)

--- pages/task-list.php
+++ pages/task-list.php
@@ -1,5 +1,8 @@
 <?php
 
+# Setting where content is allowed from to include APIs like giphy & google fonts & google
+header("Content-Security-Policy: default-src 'self'; script-src 'self' https://apis.google.com https://www.google.com/recaptcha/api.js https://www.gstatic.com/recaptcha/releases/TkacYOdEJbdB_JjX802TMer9/recaptcha__en.js; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://*.giphy.com https://giphy.com https://i.giphy.com https://www.google.com/recaptcha/api.js; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://api.giphy.com https://giphy.com https://www.google.com/recaptcha/api2/clr; frame-src 'self' https://www.google.com https://accounts.google.com https://www.google.com/recaptcha/api.js;");
+
 session_start(); # Starting the session
 
 if (empty($_SESSION['token'])) { // CSRF Token Check
--- required/database-connector.php
+++ required/database-connector.php
@@ -1,4 +1,5 @@
 <?php
+
 $server = "localhost";
 $username = "root";
 $password = "";  # By default, XAMPP password is empty
